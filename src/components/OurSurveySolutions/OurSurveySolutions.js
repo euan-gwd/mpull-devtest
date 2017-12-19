8d95549805bf41340bc3c4a2289428f53e2483be
euan-gwd <euan@amwic.co.za>
2017-12-19 15:11:18 +0200

    init

--- src/components/OurSurveySolutions/OurSurveySolutions.js
+++ src/components/OurSurveySolutions/OurSurveySolutions.js
@@ -1,22 +1,27 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import bdImage from './images/woman_telephone.png';
+import offerImage from './images/offer.png';
 
 class OurSurveySolutions extends Component {
   state = {};
   render() {
     return (
-      <BackDrop image={bdImage}>
+      <BackDrop>
         <Container>
-          <Title>We do Proven Survey Solutions</Title>
-          <Content>
-            Ro officiae conem facest, corporerum dolore, eribusd andamus voluptatem con rere parior
-            sit officae. Aximagn ienimol orporem perum, aritis modiorp ostiam fa.
-          </Content>
-          <ButtonGroup>
-            <Button>Read More</Button>
-            <Button>Customer Survey</Button>
-          </ButtonGroup>
+          <LeftColumn>
+            <Title>We have the right insider Knowledge</Title>
+            <Content>
+              Ro officiae conem facest, corporerum dolore, eribusd andamus voluptatem con rere
+              parior sit officae. Aximagn ienimol orporem perum, aritis modiorp ostiam fa.
+            </Content>
+            <ButtonGroup>
+              <Button>Read the new glex</Button>
+            </ButtonGroup>
+          </LeftColumn>
+          <RightColumn>
+            <OfferButton>Get new insights in our survey rapport</OfferButton>
+            <OfferImg src={offerImage} alt="offerImage" />
+          </RightColumn>
         </Container>
       </BackDrop>
     );
@@ -29,7 +34,7 @@ const BackDrop = styled.section`
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  background: url(${props => props.image}) no-repeat;
+  background-color: #f0f1f0;
   background-origin: border-box;
   background-size: cover;
   background-position: -3cm 0, center;
@@ -38,7 +43,7 @@ const BackDrop = styled.section`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    background: url(${props => props.image}) no-repeat;
+    background-color: #f0f1f0;
     background-origin: border-box;
     background-size: cover;
     background-position: 0 0, center;
@@ -47,21 +52,57 @@ const BackDrop = styled.section`
 
 const Container = styled.div`
   margin: 0;
-  padding: 3rem 0;
+  padding: 3rem 1rem 0;
   box-sizing: border-box;
   display: grid;
-  grid-template-rows: 0.25fr 1fr 1fr;
-  grid-template-columns: 1fr;
-  align-items: center;
+  grid-template-rows: 1fr 1fr;
 
   @media screen and (min-width: 768px) {
     margin: 0 auto;
     padding: 3rem 0;
     box-sizing: border-box;
     display: grid;
-    grid-template-rows: repeat(3, 1fr);
+    grid-template-rows: 1fr;
     grid-template-columns: repeat(6, 1fr);
     max-width: 1024px;
+  }
+`;
+
+const LeftColumn = styled.div`
+  grid-row: 2;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+  @media screen and (min-width: 768px) {
+    grid-row: 1;
+    grid-column: 1 / 4;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    display: grid;
+    grid-template-rows: repeat(4, 1fr);
+    grid-template-columns: 1fr;
+  }
+`;
+
+const RightColumn = styled.div`
+  grid-row: 1;
+  margin: 0;
+  padding: 0 2rem;
+  box-sizing: border-box;
+  height: 100%;
+  display: inline-block;
+
+  @media screen and (min-width: 768px) {
+    grid-row: 1;
+    grid-column: 4 / 7;
+    justify-self: end;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    height: 100%;
+    display: inline-block;
   }
 `;
 
@@ -69,6 +110,8 @@ const Title = styled.h1`
   margin: 1rem;
   padding: 0;
   box-sizing: border-box;
+  position: relative;
+  top: -50px;
   font-family: 'Roboto Slab', serif;
   font-weight: 400;
   font-size: 1.5rem;
@@ -77,11 +120,12 @@ const Title = styled.h1`
 
   @media screen and (min-width: 768px) {
     grid-row: 1;
-    grid-column: 4 / 7;
-    align-self: end;
+    grid-column: 1;
+    align-self: center;
     margin: 0;
-    padding: 0 6rem 0 0;
+    padding: 0;
     box-sizing: border-box;
+    position: unset;
     font-family: 'Roboto Slab', serif;
     font-weight: 400;
     font-size: 3rem;
@@ -91,7 +135,7 @@ const Title = styled.h1`
 `;
 
 const Content = styled.p`
-  margin: 1rem;
+  margin: 2rem 1rem 2rem;
   padding: 0;
   box-sizing: border-box;
   font-family: 'Roboto Slab', serif;
@@ -101,13 +145,12 @@ const Content = styled.p`
   color: black;
 
   @media screen and (min-width: 768px) {
-    grid-row: 2;
-    grid-column: 4 / 7;
-    align-self: end;
-    justify-self: start;
+    grid-row: 3;
+    grid-column: 1;
     margin: 0;
     padding: 0 3.5rem 0 0;
     box-sizing: border-box;
+    position: unset;
     font-family: 'Roboto Slab', serif;
     font-weight: 100;
     font-size: 1rem;
@@ -126,13 +169,13 @@ const ButtonGroup = styled.div`
   text-transform: uppercase;
   color: black;
   display: grid;
-  grid-row-gap: 1rem;
 
   @media screen and (min-width: 768px) {
     margin: 0;
     padding: 0;
-    grid-row: 3;
-    grid-column: 4 / 7;
+    box-sizing: border-box;
+    grid-row: 4;
+    grid-column: 1;
     display: grid;
     grid-template-columns: 1fr 1fr;
     grid-column-gap: 2rem;
@@ -140,7 +183,6 @@ const ButtonGroup = styled.div`
 `;
 
 const Button = styled.button`
-  align-self: end;
   margin: 0;
   padding: 0.25rem;
   box-sizing: border-box;
@@ -156,5 +198,77 @@ const Button = styled.button`
   &:hover {
     background-color: black;
     color: white;
+  }
+`;
+
+const OfferImg = styled.img`
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  max-width: 100%;
+  height: 35vh;
+  object-fit: cover;
+  position: relative;
+  top: -70px;
+
+  @media screen and (min-width: 768px) {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    position: unset;
+    max-width: 100%;
+    height: auto;
+    object-fit: cover;
+  }
+`;
+
+const OfferButton = styled.button`
+  position: relative;
+  z-index: 2;
+  right: -140px;
+  width: 139px;
+  height: 139px;
+  margin: 0;
+  padding: 0.5rem;
+  box-sizing: border-box;
+  font-family: 'Roboto Slab', serif;
+  font-size: 1rem;
+  font-weight: 300;
+  color: black;
+  text-transform: uppercase;
+  border: 1px solid black;
+  border-radius: 50% 50%;
+  background-color: transparent;
+  cursor: pointer;
+
+  &:hover {
+    background-color: black;
+    color: white;
+  }
+
+  @media screen and (min-width: 768px) {
+    position: relative;
+    top: -200px;
+    right: -410px;
+    width: 139px;
+    height: 139px;
+    margin: 0;
+    padding: 0.25rem;
+    box-sizing: border-box;
+    font-family: 'Roboto Slab', serif;
+    font-size: 1rem;
+    font-weight: 300;
+    color: black;
+    text-transform: uppercase;
+    border: 1px solid black;
+    border-radius: 50% 50%;
+    border-spacing: 1rem;
+    background-color: transparent;
+    cursor: pointer;
+
+    &:hover {
+      background-color: black;
+      color: white;
+    }
   }
 `;
