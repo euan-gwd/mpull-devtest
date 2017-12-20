54bfd74c0ccf9ab9158840f085595adf0209a9ad
euan-gwd <euan@amwic.co.za>
2017-12-20 10:05:59 +0200

    Add Lang Pages

---
+++ src/components/DK/Navigation/NavBar.js
@@ -0,0 +1,245 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import DropDownMenu from './DropDownMenu';
+import NavBarLang from './NavBarLang';
+import NavBarSeek from './NavBarSeek';
+
+import burger from './images/burger.svg';
+import cross from './images/cross.svg';
+import logo from './images/logo.svg';
+import language from './images/language.svg';
+import search from './images/search.svg';
+
+class NavBar extends Component {
+  state = {
+    showSolutionsDropMenu: false,
+    showInsightsDropMenu: false,
+    showAboutUsDropMenu: false,
+    showNav: 1
+  };
+
+  handleSolutionsDropMenuToggle = () => {
+    this.setState(prevState => {
+      return { showSolutionsDropMenu: !prevState.showSolutionsDropMenu };
+    });
+  };
+
+  handleSolutionsDropMenuClose = () => {
+    this.setState({ showSolutionsDropMenu: false });
+  };
+
+  handleInsightsDropMenuToggle = () => {
+    this.setState(prevState => {
+      return { showInsightsDropMenu: !prevState.showInsightsDropMenu };
+    });
+  };
+
+  handleInsightsDropMenuClose = () => {
+    this.setState({ showInsightsDropMenu: false });
+  };
+
+  handleAboutUsDropMenuToggle = () => {
+    this.setState(prevState => {
+      return { showAboutUsDropMenu: !prevState.showAboutUsDropMenu };
+    });
+  };
+
+  handleAboutUsDropMenuClose = () => {
+    this.setState({ showAboutUsDropMenu: false });
+  };
+
+  handleLanguageBtnClick = () => {
+    this.setState({ showNav: 2 });
+  };
+
+  handleSearchBtnClick = () => {
+    this.setState({ showNav: 3 });
+  };
+
+  handleCloseBtnClick = () => {
+    this.setState({ showNav: 1 });
+  };
+
+  render() {
+    const { showNav } = this.state;
+    return (
+      <ToolBar>
+        {showNav === 1 && (
+          <MainNavigation>
+            <MobileMenu>
+              <img
+                src={this.props.open ? cross : burger}
+                alt="mobile navigation menu"
+                onClick={this.props.handleMenuToggle}
+              />
+            </MobileMenu>
+            <Logo href="/">
+              <img src={logo} className="logo" alt="logo" />
+            </Logo>
+            <NavigationItems>
+              <NavigationItem onMouseLeave={this.handleSolutionsDropMenuClose}>
+                <a onClick={this.handleSolutionsDropMenuToggle}>Solutions</a>
+                <DropDownMenu show={this.state.showSolutionsDropMenu} />
+              </NavigationItem>
+              <NavigationItem>
+                <a href="/">Cases</a>
+                <div />
+              </NavigationItem>
+              <NavigationItem onMouseLeave={this.handleInsightsDropMenuClose}>
+                <a onClick={this.handleInsightsDropMenuToggle}>Insights</a>
+                <DropDownMenu show={this.state.showInsightsDropMenu} />
+              </NavigationItem>
+              <NavigationItem onMouseLeave={this.handleAboutUsDropMenuClose}>
+                <a onClick={this.handleAboutUsDropMenuToggle}>About Us</a>
+                <DropDownMenu show={this.state.showAboutUsDropMenu} />
+              </NavigationItem>
+              <NavigationItem>
+                <a href="/">Contact</a>
+                <div />
+              </NavigationItem>
+            </NavigationItems>
+            <LanguageBtn onClick={this.handleLanguageBtnClick}>
+              <img src={language} alt="change language" />
+            </LanguageBtn>
+            <SearchBtn onClick={this.handleSearchBtnClick}>
+              <img src={search} alt="search" />
+            </SearchBtn>
+          </MainNavigation>
+        )}
+        {showNav === 2 && <NavBarLang handleCloseBtnClick={this.handleCloseBtnClick} />}
+        {showNav === 3 && <NavBarSeek handleCloseBtnClick={this.handleCloseBtnClick} />}
+      </ToolBar>
+    );
+  }
+}
+
+export default NavBar;
+
+const ToolBar = styled.header`
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  box-shadow: 0px 1px 2px 0px rgba(160, 165, 170, 0.75);
+  height: 45px;
+  z-index: 5;
+  position: fixed;
+  width: 100vw;
+  top: 0;
+  background-color: white;
+`;
+
+//Main Navigation Styles
+const MainNavigation = styled.div`
+  margin: 0 auto;
+  padding: 0 1rem;
+  box-sizing: border-box;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  align-items: center;
+  height: 45px;
+
+  @media screen and (min-width: 768px) {
+    margin: 0 auto;
+    padding: 0;
+    box-sizing: border-box;
+    display: grid;
+    grid-template-columns: repeat(10, 1fr);
+    align-items: center;
+    height: 45px;
+    max-width: 1024px;
+  }
+`;
+
+const MobileMenu = styled.a`
+  display: block;
+  cursor: pointer;
+
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
+`;
+
+const Logo = styled.a`
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  justify-self: center;
+  grid-column: 2 / span 2;
+
+  @media screen and (min-width: 768px) {
+    grid-column: 1;
+    justify-self: center;
+    align-self: start;
+    padding: 1rem 0;
+  }
+`;
+
+const NavigationItems = styled.div`
+  display: none;
+
+  @media screen and (min-width: 768px) {
+    grid-column-start: 3;
+    grid-column-end: 9;
+    align-self: center;
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-auto-row: column;
+    align-items: center;
+  }
+`;
+
+const NavigationItem = styled.div`
+  display: none;
+
+  @media screen and (min-width: 768px) {
+    margin: 0;
+    padding: 1rem 0;
+    box-sizing: border-box;
+    align-self: start;
+    justify-self: center;
+    text-align: center;
+    display: block;
+
+    a {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      color: #000;
+      text-decoration: none;
+    }
+
+    a:hover,
+    a:active,
+    a:visited {
+      border-bottom: 1px solid black;
+    }
+  }
+`;
+
+const LanguageBtn = styled.a`
+  @media screen and (max-width: 767px) {
+    display: block;
+    justify-self: end;
+    cursor: pointer;
+  }
+
+  @media screen and (min-width: 768px) {
+    grid-column: 9 / 10;
+    justify-self: end;
+    align-self: start;
+    padding: 1rem 0;
+  }
+`;
+
+const SearchBtn = styled.a`
+  @media screen and (max-width: 767px) {
+    display: none;
+  }
+
+  @media screen and (min-width: 768px) {
+    grid-column: 10 / 10;
+    justify-self: center;
+    align-self: start;
+    padding: 1rem 0;
+  }
+`;
---
+++ src/components/NO/WhoWeWorkWith/WhoWeWorkWith.js
@@ -0,0 +1,197 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import clients from './images/clients.png';
+import ButtonDark from '../Buttons/ButtonDark';
+
+class WhoWeWorkWith extends Component {
+  state = {};
+  render() {
+    return (
+      <BackDrop>
+        <Container>
+          <LeftColumn>
+            <ImgTable src={clients} alt="offerImage" />
+          </LeftColumn>
+          <RightColumn>
+            <Title>Who We Work With</Title>
+            <Content>
+              Ro officiae conem facest, corporerum dolore, eribusd andamus voluptatem con rere
+              parior sit officae. Aximagn ienimol orporem perum, aritis modiorp ostiam fa.
+            </Content>
+            <ButtonGroup>
+              <ButtonDark>Read more</ButtonDark>
+            </ButtonGroup>
+          </RightColumn>
+        </Container>
+      </BackDrop>
+    );
+  }
+}
+
+export default WhoWeWorkWith;
+
+const BackDrop = styled.section`
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background-color: #fff;
+  background-origin: border-box;
+  background-size: cover;
+
+  @media screen and (min-width: 768px) {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    background-color: #fff;
+    background-origin: border-box;
+    background-size: cover;
+    background-position: 0 0, center;
+  }
+`;
+
+const Container = styled.div`
+  margin: 0;
+  padding: 3rem 1rem;
+  box-sizing: border-box;
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+  grid-template-columns: 1fr;
+
+  @media screen and (min-width: 768px) {
+    margin: 0 auto;
+    padding: 3rem 0;
+    box-sizing: border-box;
+    display: grid;
+    grid-template-rows: 1fr;
+    grid-template-columns: repeat(6, 1fr);
+    max-width: 1024px;
+  }
+`;
+
+const LeftColumn = styled.div`
+  grid-row: 1;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  height: 100%;
+
+  @media screen and (min-width: 768px) {
+    grid-row: 1;
+    grid-column: 1 / 4;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    height: 100%;
+    display: grid;
+  }
+`;
+
+const RightColumn = styled.div`
+  grid-row: 2;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+  @media screen and (min-width: 768px) {
+    grid-row: 1;
+    grid-column: 4 / 7;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    display: grid;
+    grid-template-rows: repeat(3, 1fr);
+    grid-template-columns: 1fr;
+  }
+`;
+
+const Title = styled.h1`
+  margin: 0;
+  padding: 0 1rem;
+  box-sizing: border-box;
+  font-family: 'Roboto Slab', serif;
+  font-weight: 400;
+  font-size: 1.5rem;
+  text-transform: uppercase;
+  color: black;
+
+  @media screen and (min-width: 768px) {
+    grid-row: 1;
+    grid-column: 1;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto Slab', serif;
+    font-weight: 400;
+    font-size: 3rem;
+    text-transform: uppercase;
+    color: black;
+  }
+`;
+
+const Content = styled.p`
+  margin: 2rem 1rem 2rem;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Roboto Slab', serif;
+  font-weight: 100;
+  font-size: 1rem;
+  line-height: 1.5rem;
+  color: black;
+
+  @media screen and (min-width: 768px) {
+    grid-row: 2;
+    grid-column: 1;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto Slab', serif;
+    font-weight: 100;
+    font-size: 1rem;
+    line-height: 1.5rem;
+    color: black;
+  }
+`;
+
+const ButtonGroup = styled.div`
+  margin: 0 1rem;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Roboto Slab', serif;
+  font-weight: 400;
+  font-size: 1.5rem;
+  text-transform: uppercase;
+  color: black;
+  display: grid;
+
+  @media screen and (min-width: 768px) {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    grid-row: 3;
+    grid-column: 1;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+  }
+`;
+
+const ImgTable = styled.img`
+  grid-row: 1;
+  grid-column: 1;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  max-width: 100%;
+  height: auto;
+  object-fit: cover;
+
+  @media screen and (min-width: 768px) {
+    grid-row: 1;
+    grid-column: 1;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    max-width: 100%;
+    height: auto;
+    object-fit: cover;
+  }
+`;
