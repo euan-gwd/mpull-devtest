b66fea167c5d525ac965ee6b11089b4f4b724ac5
euan-gwd <euan@amwic.co.za>
2017-12-28 21:05:10 +0200

    SP 3

--- src/components/UK/MeasureWorldWide/MeasureWorldWide.js
+++ src/components/UK/MeasureWorldWide/MeasureWorldWide.js
@@ -49,8 +49,10 @@ const Container = styled.div`
   grid-template-columns: 1fr;
 
   @media screen and (min-width: 768px) and (max-width: 1024px) {
+    margin: 0;
     padding: 2rem;
     box-sizing: border-box;
+    grid-row-gap: 2rem;
   }
 
   @media screen and (min-width: 1025px) {
@@ -72,6 +74,15 @@ const LeftColumn = styled.div`
   height: 100%;
   display: grid;
 
+  @media screen and (min-width: 768px) and (max-width: 1024px) {
+    grid-row: 1;
+    margin: 0 auto;
+    padding: 0;
+    box-sizing: border-box;
+    height: 100%;
+    display: grid;
+  }
+
   @media screen and (min-width: 1025px) {
     grid-row: 1;
     grid-column: 1 / 4;
@@ -88,6 +99,18 @@ const RightColumn = styled.div`
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+
+  @media screen and (min-width: 768px) and (max-width: 1024px) {
+    grid-row: 2;
+    margin: 0;
+    padding: 0 0;
+    box-sizing: border-box;
+    display: grid;
+    grid-template-rows: repeat(3, 1fr);
+    grid-template-columns: 1fr 1fr;
+    justify-content: center;
+    align-items: center;
+  }
 
   @media screen and (min-width: 1025px) {
     grid-row: 1;
@@ -111,6 +134,20 @@ const Title = styled.h1`
   font-size: 2rem;
   text-transform: uppercase;
   color: black;
+
+  @media screen and (min-width: 768px) and (max-width: 1024px) {
+    grid-row: 1;
+    grid-column: 1;
+    align-self: center;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto Slab', serif;
+    font-weight: 400;
+    font-size: 3rem;
+    text-transform: uppercase;
+    color: black;
+  }
 
   @media screen and (min-width: 1025px) {
     grid-row: 1;
@@ -138,6 +175,19 @@ const Content = styled.p`
   line-height: 1.5rem;
   color: black;
 
+  @media screen and (min-width: 768px) and (max-width: 1024px) {
+    grid-row: 2;
+    grid-column: 1;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto Slab', serif;
+    font-weight: 100;
+    font-size: 1rem;
+    line-height: 1.5rem;
+    color: black;
+  }
+
   @media screen and (min-width: 1025px) {
     grid-row: 2;
     grid-column: 1;
@@ -162,6 +212,16 @@ const ButtonGroup = styled.div`
   text-transform: uppercase;
   color: black;
   display: grid;
+
+  @media screen and (min-width: 768px) and (max-width: 1024px) {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    grid-row: 3;
+    grid-column: 1;
+    display: grid;
+    grid-template-columns: 1fr;
+  }
 
   @media screen and (min-width: 1025px) {
     margin: 0;
@@ -191,8 +251,8 @@ const WorldImg = styled.img`
     padding: 0;
     box-sizing: border-box;
     max-width: 100%;
-    height: 30vh;
-    object-fit: contain;
+    height: auto;
+    object-fit: cover;
   }
 
   @media screen and (min-width: 1025px) {
@@ -229,6 +289,31 @@ const WorldButton = styled.button`
   &:hover {
     background-color: black;
     color: white;
+  }
+
+  @media screen and (min-width: 768px) and (max-width: 1024px) {
+    grid-row: 1;
+    grid-column: 1;
+    z-index: 2;
+    width: 105px;
+    height: 105px;
+    margin: 0;
+    padding: 0.5rem;
+    box-sizing: border-box;
+    font-family: 'Roboto Slab', serif;
+    font-size: 1rem;
+    font-weight: 300;
+    color: black;
+    text-transform: uppercase;
+    border: 1px solid black;
+    border-radius: 100%;
+    background-color: transparent;
+    cursor: pointer;
+
+    &:hover {
+      background-color: black;
+      color: white;
+    }
   }
 
   @media screen and (min-width: 1025px) {
