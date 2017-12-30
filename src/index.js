c721180686fd0fc2fa0ddb5c5217d53eb5747734
euan-gwd <euan@amwic.co.za>
2017-12-30 18:13:27 +0200

    Cleanup Index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import './reset.css';
 import './index.css';
-import HomeUK from './components/UK/Home';
-
-import registerServiceWorker from './registerServiceWorker';
-
 import './i18n';
+import HomeUK from './components/UK/Home';
+import registerServiceWorker from './registerServiceWorker';
 
 const Root = () => {
   return (
